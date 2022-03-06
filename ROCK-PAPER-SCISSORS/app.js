adeccf5a2b010b71b317016446f21e1b2c325b1b
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-06 13:33:16 -0600

    RPS (rock paper scissors) js edits - eventListener for buttons

--- ROCK-PAPER-SCISSORS/app.js
+++ ROCK-PAPER-SCISSORS/app.js
@@ -3,3 +3,5 @@ const computerChoice = document.getElementById('computer-choice');
 const userChoice = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', () =>))
