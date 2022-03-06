1ce554ab6db7fe1d7e087b4350f55171b86c5211
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-06 13:36:10 -0600

    RPS (rock paper scissors) js edits - renaming '...Choice' vars to '...ChoiceDisplay'

--- ROCK-PAPER-SCISSORS/app.js
+++ ROCK-PAPER-SCISSORS/app.js
@@ -1,7 +1,9 @@
 "use strict";
-const computerChoice = document.getElementById('computer-choice');
-const userChoice = document.getElementById('user-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
+let userChoice
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', () =>))
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {userChoice = e.target.id
+}))
